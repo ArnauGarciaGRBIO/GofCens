8b1da51eec386052c6698b92fd3e284eedf95978
ArnauGarciaGRBIO <arnau.garcia.fernandez@upc.edu>
2024-10-23 10:11:22 +0200

    Updates to silence warnings and notes.

--- R/plot.kmPlot.R
+++ R/plot.kmPlot.R
@@ -28,9 +28,9 @@ plot.kmPlot <- function(x, ...) {
             lower = x$survKM$lower,
             upper = x$survKM$upper
           )
-          p1 <- ggplot(data = km_data, aes(x = time, y = surv)) +
+          p1 <- ggplot(data = km_data, aes(x = km_data$time, y = km_data$surv)) +
             geom_step(color = x$colour[2], linewidth = 1) +
-            geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2,
+            geom_ribbon(aes(ymin = km_data$lower, ymax = km_data$upper), alpha = 0.2,
                         fill = x$colour[3]) +
             labs(
               title = x$titl[[i]],
@@ -42,8 +42,8 @@ plot.kmPlot <- function(x, ...) {
               axis.title = element_text(size = 8),
               axis.text = element_text(size = 6)
             ) +
-            geom_point(aes(x, y), size = 0.5, data = tmpdat) +
-            geom_line(aes(x, y), data = tmpdat, linewidth = 0.5,
+            geom_point(aes(tmpdat$x, tmpdat$y), size = 0.5, data = tmpdat) +
+            geom_line(aes(tmpdat$x, tmpdat$y), data = tmpdat, linewidth = 0.5,
                       color = x$colour[1])
         })
       }
